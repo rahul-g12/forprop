c28d79eee47779e08d84ef8b819c92913b3ff075
rahul-g12 <rahul.govindasamy@ainqa.com>
2021-06-18 18:30:40 +0530

    any

---
+++ Session2.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Session2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   //just now
+
+		System.out.println("Enter the Input: ");
+		
+		Scanner scans = new Scanner(System.in);
+		int nUserInput = scans.nextInt();
+		
+		int nRowCol = (nUserInput+nUserInput)-1;
+		
+		int nLeft = nUserInput, nRight = nUserInput;
+		//System.out.println("nRowCol : "+nRowCol);
+		
+		//new
+		for(int i=1;i<=nRowCol;i++)//row
+		{
+			
+			for(int j=1; j<=nRowCol;j++)//col
+			{
+				
+				if(j>=nLeft && j<=nRight )
+				    System.out.print("*");
+				else
+					System.out.print(" Thasleema");
+				
+			}
+			if(i<nUserInput) {
+			   nLeft--;
+			   nRight++;
+			}
+			else {
+				nLeft++;
+				nRight--;
+			}
+			
+			
+			System.out.println();
+			System.out.println("sreehari";
+			
+		}
+		
+		
+		
+	}
+
+}
